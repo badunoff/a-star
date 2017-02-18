3c7b9acabeebfbcc9fdb92a99a09d36eb86702f4
Ryan Dunn <ryanmdunn@optonline.net>
2017-02-18 16:24:18 -0500

    Added PathComputer.java, Added StepNode.java, made MapNode.java a comparable

---
+++ src/MapCell.java
@@ -0,0 +1,87 @@
+import java.awt.Point;
+
+
+public class MapCell implements Comparable<MapCell>{
+	private boolean open;
+	private int search;
+	private int g;
+	private int h;
+	private Point location;
+	private Point cameFrom;
+	
+	public MapCell(Point pt){
+		location = pt;
+		this.open = true;
+		this.search = 0;
+		cameFrom = null;
+	}
+	
+	public void setOpen(boolean open){
+		this.open = open;
+	}
+	
+	public boolean isOpen(){
+		return open;
+	}
+	
+	public String toString(){
+		return (open) ? "   ." : "||||";
+	}
+	
+	public int getG(){
+		return g;
+	}
+	
+	public void setG(int g){
+		this.g = g;
+	}
+	
+	public int getH()
+	{
+		return h;
+	}
+	
+	public void setH(int h)
+	{
+		this.h = h;
+	}
+	
+	public Point getCameFrom(){
+		return cameFrom;
+	}
+	
+	public void setCameFrom(Point pt){
+		cameFrom = pt;
+	}
+	
+	public Point getLocation(){
+		return location;
+	}
+	
+	public void setLocation(Point pt){
+		location = pt;
+	}
+	
+	public int getSearch(){
+		return search;
+	}
+	
+	public void setSearch(int search){
+		this.search = search;
+	}
+	
+	public int compareTo(MapCell other) throws ClassCastException
+	{
+		if (!(other instanceof MapCell))
+		      throw new ClassCastException("A Person object expected.");
+		int otherH = ((MapCell)other).getH();
+		int otherG = ((MapCell)other).getG();
+		if((this.h + this.g) > (otherH + otherG))
+			return 1;
+		if((this.h + this.g) < (otherH + otherG))
+			return -1;
+		if(this.g > otherG)
+			return 1;
+		return -1;
+	}
+}
---
+++ src/PathComputer.java
@@ -0,0 +1,101 @@
+import java.awt.Point;
+import java.lang.reflect.Array;
+
+public class PathComputer {
+	
+	public StepNode ComputePath(MapCell[][] map, Point start, Point end, int counter)
+	{
+		BinaryHeap<MapCell> openList = new BinaryHeap<MapCell>();
+		map[start.x][start.y].setG(0);
+		map[start.x][start.y].setSearch(counter);
+		map[end.x][end.y].setG(Integer.MAX_VALUE);
+		map[end.x][end.y].setSearch(counter);
+		MapCell current = map[start.x][start.y];
+		current.setH(Math.abs(start.x - end.x)+Math.abs(start.y - end.y));
+		while(map[end.x][end.y].getG() > (current.getG() + current.getH()))
+		{
+			int currX = current.getLocation().x;
+			int currY = current.getLocation().y;
+			if(currX > 1 && map[currX-1][currY].isOpen()) //left
+			{
+				if(map[currX-1][currY].getSearch() < counter)
+				{
+					map[currX-1][currY].setSearch(counter);
+					map[currX-1][currY].setG(Integer.MAX_VALUE);
+					map[currX-1][currY].setH(Math.abs(map[currX-1][currY].getLocation().x - end.x)+Math.abs(map[currX-1][currY].getLocation().y - end.y));
+				}
+				if(map[currX-1][currY].getG() > (current.getG()+1))
+				{
+					map[currX-1][currY].setG(current.getG()+1);
+					map[currX-1][currY].setCameFrom(current.getLocation());
+					//if it's in the open list, remove it (ADD HERE)
+					openList.push(map[currX-1][currY]);
+				}
+			}
+				
+			if(currY < Array.getLength(map) - 2 && map[currX][currY+1].isOpen()) //up
+			{
+				if(map[currX-1][currY].getSearch() < counter)
+				{
+					map[currX][currY+1].setSearch(counter);
+					map[currX][currY+1].setG(Integer.MAX_VALUE);
+					map[currX][currY+1].setH(Math.abs(map[currX][currY+1].getLocation().x - end.x)+Math.abs(map[currX][currY+1].getLocation().y - end.y));
+				}
+				if(map[currX][currY+1].getG() > (current.getG()+1))
+				{
+					map[currX][currY+1].setG(current.getG()+1);
+					map[currX][currY+1].setCameFrom(current.getLocation());
+					//if it's in the open list, remove it (ADD HERE)
+					openList.push(map[currX][currY+1]);
+				}
+			}
+				
+			if(currX < Array.getLength(map) - 2 && map[currX+1][currY].isOpen()) //right
+			{
+				if(map[currX+1][currY].getSearch() < counter)
+				{
+					map[currX+1][currY].setSearch(counter);
+					map[currX+1][currY].setG(Integer.MAX_VALUE);
+					map[currX+1][currY].setH(Math.abs(map[currX+1][currY].getLocation().x - end.x)+Math.abs(map[currX+1][currY].getLocation().y - end.y));
+				}
+				if(map[currX+1][currY].getG() > (current.getG()+1))
+				{
+					map[currX+1][currY].setG(current.getG()+1);
+					map[currX+1][currY].setCameFrom(current.getLocation());
+					//if it's in the open list, remove it (ADD HERE)
+					openList.push(map[currX+1][currY]);
+				}
+			}
+				
+			if(currY > 1 && map[currX][currY-1].isOpen()) //down
+			{
+				if(map[currX][currY-1].getSearch() < counter)
+				{
+					map[currX][currY-1].setSearch(counter);
+					map[currX][currY-1].setG(Integer.MAX_VALUE);
+					map[currX][currY-1].setH(Math.abs(map[currX][currY-1].getLocation().x - end.x)+Math.abs(map[currX][currY-1].getLocation().y - end.y));
+				}
+				if(map[currX][currY-1].getG() > (current.getG()+1))
+				{
+					map[currX][currY-1].setG(current.getG()+1);
+					map[currX][currY-1].setCameFrom(current.getLocation());
+					//if it's in the open list, remove it (ADD HERE)
+					openList.push(map[currX][currY-1]);
+				}
+			}
+			current = openList.pop();
+		}
+		if(openList.size() == 0)
+			return null;
+		StepNode prev = new StepNode(map[end.x][end.y].getLocation(), null);
+		StepNode curr = null;
+		current = map[end.x][end.y];
+		while(curr.Location != map[start.x][start.y].getLocation())
+		{
+			curr = new StepNode(current.getCameFrom(), prev);
+			prev = curr;
+			current = map[current.getCameFrom().x][current.getCameFrom().y];
+		}
+		return curr;
+	}
+}
