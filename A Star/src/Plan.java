02efdc491f00e9de1f1404992e53b0a52334dd52
badunoff <badunoff@yahoo.com>
2017-02-19 13:18:51 -0500

    Plan object and enum

---
+++ A Star/src/Plan.java
@@ -0,0 +1,24 @@
+import java.awt.Point;
+import java.util.LinkedList;
+
+
+public class Plan {
+	private PlanType type;
+	private LinkedList<Point> list;
+	
+	public Plan(LinkedList<Point> list, PlanType type){
+		this.list = list;
+		this.type = type;
+	}
+	
+	public Point remove(){
+		switch(type){
+			case QUEUE:
+				return list.removeFirst();
+			case STACK:
+				return list.removeLast();
+			default:
+				return null;
+		}
+	}
+}
